cdf3bf0d5cefd108a71e6ecd8a0102cf0786912a
nlee096 <72417519+nlee096@users.noreply.github.com>
2023-06-12 23:04:26 -0700

    ready to run
    accuracy now output in % 
    normalization function should work now
    outputs match example more or less
    can now handle more than 2 different classes
    all test cases pass

--- project2.cpp
+++ project2.cpp
@@ -18,7 +18,7 @@ int num_features;
 int num_instances;
 const int MAX_INSTANCES = 4000;
 const int MAX_FEATURES = 80;
-double data[MAX_INSTANCES][MAX_FEATURES];
+double data[MAX_INSTANCES][MAX_FEATURES+1];
 
 void normalize(){
 	double sum;
@@ -26,28 +26,27 @@ void normalize(){
 	double mean[num_features];
 	double std[num_features];
 	cout << "Normalizing ... " << endl;
-	for(int i = 1; i < num_features; i++){
+	for(int i = 1; i < num_features+1; i++){
 		sum = 0;
 		for(int j = 0; j < num_instances; j++){
 			sum += data[j][i];
 		}
-		mean[i] = sum / (double)num_instances;
+		mean[i-1] = sum / (double)num_instances;
 	}
-	
 
 	double tempMean;
-	for(int i = 1; i < num_features; i++){
+	for(int i = 1; i < num_features+1; i++){
 		variance = 0;
-		tempMean = mean[i];
+		tempMean = mean[i-1];
 		for(int j = 0; j < num_instances; j++){
 			variance += pow(data[j][i] - tempMean, 2);
 		}
-		std[i] = sqrt(variance/ (double)num_instances);
+		std[i-1] = sqrt(variance/ (double)(num_instances-1));
 	}
 
 	for(int i = 0; i < num_instances; i++){
-		for(int j = 1; j < num_features; j++){
-			data[i][j] = (data[i][j] - mean[j]) / std[j];
+		for(int j = 1; j < num_features+1; j++){
+			data[i][j] = (data[i][j] - mean[j-1]) / std[j-1];
 		}
 	}
 	cout << "Done" << endl;
@@ -60,15 +59,14 @@ double cross_validation(set<int> &feature_set){
 	int correct = 0;
 
 	if(feature_set.empty()){
-        // below is for the case that we do not know the number of classifications
-        /*
+        // below is for the case that we do not know the number of classifications 
 		vector <int> common;
 		vector <int> occurance;
 		common.push_back(data[0][0]);
 		occurance.push_back(1);
 		int label;
 		bool newLabel;
-		for(int i = 0; i < num_instances; i++){
+		for(int i = 1; i < num_instances; i++){
 			label = data[i][0];
 			newLabel = true;
 			for(int j = 0; j < common.size(); j++){
@@ -91,22 +89,21 @@ double cross_validation(set<int> &feature_set){
 				index = i;
 			}
 		}
-        */
-
+        
         // code below assumes only 2 classifications (1, 2)
-        int one = 0;
-        int two = 0;
-        int label;
-        for(int i = 0; i < num_instances; i++){
-            label = data[i][0];
-            if(label == 1){
-                one++;
-            }
-            else{
-                two++;
-            }
-        }
-        int max = (one >= two)? one : two;
+        // int one = 0;
+        // int two = 0;
+        // int label;
+        // for(int i = 0; i < num_instances; i++){
+        //     label = data[i][0];
+        //     if(label == 1){
+        //         one++;
+        //     }
+        //     else{
+        //         two++;
+        //     }
+        // }
+        // int max = (one >= two)? one : two;
 		return (double)max/(double)num_instances;
 	}	
 
@@ -171,7 +168,7 @@ double cross_validation(){
 */
 
 void forward_search(/*no input needed for now*/){
-	//auto ss = high_resolution_clock::now();
+	auto ss = high_resolution_clock::now();
     set<int> best_set;
     double best_acc = cross_validation(best_set);
     set<int> current_set;
@@ -179,14 +176,15 @@ void forward_search(/*no input needed for now*/){
     set<int>::iterator it;
     set<int>::iterator temp;
     
-    cout << "Using no features I get an accuracy of " << best_acc << "%"<< endl;
+    // cout << "Using no features I get an accuracy of " << (best_acc*100) << "%"<< endl;
+    cout << "Running nearest neighbor with no features, using \"Leave-one-out\" evaluation, I get an accuracy of " << (best_acc*100) << "%"<< endl;
     cout << "Beginning search" << endl << endl;
     
     double last_acc = best_acc;
     
     for(int i = 1; i <= num_features; i++){
-    	//auto step = high_resolution_clock::now();
-        //cout << "on the " << i << " level of the search tree." << endl;
+    	// auto step = high_resolution_clock::now();
+        // cout << "on the " << i << " level of the search tree." << endl;
         int add_feature = 0;
         double curr_best = 0;
         
@@ -216,16 +214,16 @@ void forward_search(/*no input needed for now*/){
             //auto duration = duration_cast<microseconds>(stop - start);
 
             //cout << accuracy << "%. Time : " << duration.count() << " microseconds" << endl;
-            cout << accuracy << "%" << endl;
+            cout << (accuracy*100) << "%" << endl;
             
             if(accuracy > curr_best){
                 curr_best = accuracy;
                 add_feature=j;
             }
         }
-        //auto halt = high_resolution_clock::now();
-        //auto time = duration_cast<microseconds>(halt - step);
-        //cout << time.count() << " microseconds" << endl;
+        // auto halt = high_resolution_clock::now();
+        // auto time = duration_cast<microseconds>(halt - step);
+        // cout << time.count() << " microseconds" << endl;
         
         current_set.insert(add_feature);
         //cout << "On level " << i << " feature " << add_feature <<  " was added to current set" << endl << endl;
@@ -259,14 +257,14 @@ void forward_search(/*no input needed for now*/){
             cout << ", ";
         }
     }
-    cout << "}, which has an accuracy of " << best_acc << "%" << endl;
-    //auto se = high_resolution_clock::now();
-    //auto total = duration_cast<microseconds>(se - ss);
-    //cout << total.count() << " microseconds" << endl;
+    cout << "}, which has an accuracy of " << (best_acc*100) << "%" << endl;
+    auto se = high_resolution_clock::now();
+    auto total = duration_cast<microseconds>(se - ss);
+    cout << "total time : " << (double(total.count()) / 1000000) << " seconds" << endl;
 }
 
 void back_elim(/*no input needed for now*/){
-	//auto ss = high_resolution_clock::now();
+	auto ss = high_resolution_clock::now();
     set<int> best_set;
     for(int i = 1; i <= num_features; i++){
         best_set.insert(i);
@@ -274,16 +272,17 @@ void back_elim(/*no input needed for now*/){
     
     set<int> current_set = best_set;
 
-    //auto aft_start = high_resolution_clock::now();
+    auto aft_start = high_resolution_clock::now();
     double best_acc = cross_validation(current_set);
-    //auto aft_stop = high_resolution_clock::now();
-    //auto aft_time = duration_cast<microseconds>(aft_stop - aft_start);
+    auto aft_stop = high_resolution_clock::now();
+    auto aft_time = duration_cast<microseconds>(aft_stop - aft_start);
     
     set<int>::iterator it;
     set<int>::iterator temp;
     
-    cout << "Using all features I get an accuracy of " << best_acc << "%"<< endl;
-    //cout << "time : " << aft_time.count() << endl;
+    // cout << "Using all features I get an accuracy of " << (best_acc*100) << "%"<< endl;
+    cout << "Running nearest neighbor with all "<< num_features << " features, using \"Leave-one-out\" evaluation, I get an accuracy of " << (best_acc*100) << "%"<< endl;
+    cout << "time : " << (double(aft_time.count()) / 1000000) << " seconds" << endl;
     cout << "Beginning search" << endl << endl;
     
     double last_acc = best_acc;
@@ -321,7 +320,7 @@ void back_elim(/*no input needed for now*/){
             //auto duration = duration_cast<microseconds>(stop - start);
             
             //cout << accuracy << "%. Time : " << duration.count() << " microseconds" << endl;
-            cout << accuracy << "%" << endl;
+            cout << (accuracy*100) << "%" << endl;
             
             if(accuracy > curr_best){
                 curr_best = accuracy;
@@ -365,35 +364,56 @@ void back_elim(/*no input needed for now*/){
             cout << ", ";
         }
     }
-    cout << "}, which has an accuracy of " << best_acc << "%" << endl;
-    //auto se = high_resolution_clock::now();
-    //auto total = duration_cast<microseconds>(se - ss);
-    //cout << total.count() << " microseconds" << endl;
+    cout << "}, which has an accuracy of " << (best_acc*100) << "%" << endl;
+    auto se = high_resolution_clock::now();
+    auto total = duration_cast<microseconds>(se - ss);
+    cout << "total time : " << (double(total.count()) / 1000000) << " seconds" << endl;
 }
 
 int main()
 {
-    cout << "Welcome to Nathan Lee's Feature Selection Algorithm." << endl;
+    cout << "Welcome to Nathan Lee's and Sahas Poyekar's Feature Selection Algorithm." << endl;
     cout << "Select which data set:" << endl;
-    cout << "1. small-test-dataset.txt" << endl;
-    cout << "2. CS170_XXXlarge_Data__1.txt" << endl;
-    cout << "3. CS170_small_Data__32.txt" << endl;
-    cout << "4. CS170_large_Data__32.txt" << endl;
+    cout << "1. Test small dataset 32" << endl;
+    cout << "2. Test small dataset 33" << endl;
+    cout << "3. Test large dataset 32" << endl;
+    cout << "4. Test large dataset 33" << endl;
+    cout << "5. CS170_small_Data__6.txt" << endl;
+    cout << "6. CS170_large_Data__4.txt" << endl;
+    cout << "7. CS170_XXXlarge_Data__10.txt" << endl;
+    cout << "8. Type in your own" << endl;
+    // cout << "9. real world dataset" << endl;
+
     int fileOption;
     string fileName;
     cin >> fileOption;
     if(fileOption == 1){
-    	fileName = "small-test-dataset.txt";
-    }
-    else if(fileOption ==2){
-    	fileName = "CS170_XXXlarge_Data__1.txt";
-    }
-    else if(fileOption ==3){
     	fileName = "CS170_small_Data__32.txt";
     }
-    else if(fileOption ==4){
+    else if(fileOption ==2){
+    	fileName = "CS170_small_Data__33.txt";
+    }
+    else if(fileOption ==3){
     	fileName = "CS170_large_Data__32.txt";
     }
+    else if(fileOption ==4){
+    	fileName = "CS170_large_Data__33.txt";
+    }
+    else if(fileOption ==5){
+        fileName = "CS170_small_Data__6.txt";
+    }
+    else if(fileOption ==6){
+        fileName = "CS170_large_Data__4.txt";
+    }
+    else if(fileOption ==7){
+        fileName = "CS170_XXXlarge_Data__10.txt";
+    }
+    else if(fileOption ==8){
+        cin >> fileName;
+    }
+    // else if(fileOption ==8){
+    //     fileName = "data.txt";
+    // }
     else{
     	cout << "invlid file input" << endl;
     	exit(1);
@@ -409,7 +429,7 @@ int main()
     int feature_num = 0;
     double value;
     if(file.is_open()){
-    	cout << "Reading ..." << endl;
+    	// cout << "Reading ..." << endl;
     	while(!file.eof()){
     	    feature_num = 0;
             getline(file, instance);
@@ -426,18 +446,13 @@ int main()
             instance_num++;
     	}
     }
-    //cout << num_features << " " << instance_num << endl;
+
     num_instances = instance_num - 1;
     num_features--;
-    cout << "Done" << endl;
-    cout << num_instances << " instances" << endl;
-    cout << num_features << " features" << endl;
-    //cout << "Please enter total number of features: ";
-    //cin >> num_features;   
-    if(num_features<0){
-        cout << "invalid input" << endl;
-        exit(1);
-    }
+    // cout << "Done" << endl;
+    // cout << num_instances << " instances" << endl;
+    // cout << num_features << " features" << endl;
+    cout << "This dataset has " << num_features << " features (not including the class attribute), with " << num_instances << " instance." << endl;
 
     cout << "Do you want to normalize? (0 = no, 1 = yes)" << endl;
     int norm;
@@ -449,10 +464,10 @@ int main()
     	cout << "invalid input" << endl;
     	exit(1);
     }
+
     cout << "Type the number of the algorithm you want to run." << endl
         << "1 : Forward Selection" << endl
-        << "2 : Backward Elimination" << endl
-        << "3 : Nathan's Special algorithm (not implemented. Does nothing)" << endl;
+        << "2 : Backward Elimination" << endl;
     
     int option;
     cin >> option;
